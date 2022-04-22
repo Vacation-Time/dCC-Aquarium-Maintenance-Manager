beb3118f9366c247b2ca6b0409f6eb2e3bab4821
The Luca <80943931+Vacation-Time@users.noreply.github.com>
2022-04-22 16:42:48 -0700

    Upated Backend and MySQL With Working Postman Tests

--- backend/drf_jwt_backend/tank/serializers.py
+++ backend/drf_jwt_backend/tank/serializers.py
@@ -7,6 +7,6 @@ from .models import Tank
 class TankSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tank
-        fields = ['id', 'user_id', 'tank_name', 'water_type', 'tank_size', 'water_temp_chk_last_done', 'value', 'next_due', 'water_change_last_done', 'amount_%', 'next_due',
-                  'plant_care_last_done', 'next_due', 'water_level_chk_last_done', 'ph_level', 'nitrite_level', 'nitrate_level', 'o2_level', 'next_due', 'notes']
+        fields = ['id', 'tankName', 'waterType', 'tankSize', 'waterTempChkLastDone', 'value', 'nextDue', 'waterChangeLastDone', 'amountPercent', 'nextDue',
+                  'plantCareLastDone', 'nextDue', 'waterLevelsChkLastDone', 'phLevel', 'nitriteLevel', 'nitrateLevel', 'o2Level', 'nextDue', 'notes']
         depth = 1
